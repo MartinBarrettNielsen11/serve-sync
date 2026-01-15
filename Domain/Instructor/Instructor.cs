0cd1afda8938acd6ab89dbbb687b3cc5778a054b
MartinBarrettNielsen11 <76744292+MartinBarrettNielsen11@users.noreply.github.com>
2026-01-15 21:31:09 +0100

    upgrade to .net 10

--- Domain/Instructor/Instructor.cs
+++ Domain/Instructor/Instructor.cs
@@ -5,5 +5,5 @@ public class Instructor
     private readonly Guid _id;
     private readonly Guid _userId;
     private readonly List<Guid> _sessionIds = new();
-    private readonly Schedule.Schedule _schedule = Schedule.Empty();
+    //private readonly Schedule.Schedule _schedule = Schedule.Empty();
 }
--- Domain/Player/Player.cs
+++ Domain/Player/Player.cs
@@ -3,6 +3,6 @@ namespace domain.Player;
 public class Player
 {
     private readonly Guid _userId;
-    private readonly Schedule.Schedule _schedule = Schedule.Empty();
+    //private readonly Schedule.Schedule _schedule = Schedule.Empty();
     private readonly List<Guid> _sessionIds = new();   
 }
