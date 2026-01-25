99c911ba70f2611bf1606e38b1bc91b0fcb6aaff
MartinBarrettNielsen11 <76744292+MartinBarrettNielsen11@users.noreply.github.com>
2026-01-25 22:50:37 +0100

    remove prior "sln" solution format

--- SharedKernel/obj/Debug/net10.0/SharedKernel.AssemblyInfo.cs
+++ SharedKernel/obj/Debug/net10.0/SharedKernel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SharedKernel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54c64a513c85d5fa72af8ce9b3588e4dc3179621")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53d5e7a984466469c9cc1605d538a7908b0dbb16")]
 [assembly: System.Reflection.AssemblyProductAttribute("SharedKernel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SharedKernel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
