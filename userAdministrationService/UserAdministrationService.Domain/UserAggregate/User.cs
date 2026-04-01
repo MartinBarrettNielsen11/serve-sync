c057eead76ee5bb290137e5e584126ea1db69d72
MartinBarrettNielsen11 <76744292+MartinBarrettNielsen11@users.noreply.github.com>
2026-04-01 20:00:12 +0200

    add constructor for User entity

--- userAdministrationService/UserAdministrationService.Domain/UserAggregate/User.cs
+++ userAdministrationService/UserAdministrationService.Domain/UserAggregate/User.cs
@@ -1,6 +1,8 @@
-﻿namespace UserAdministrationService.Domain.UserAggregate;
+﻿using SharedKernel;
 
-public class User
+namespace UserAdministrationService.Domain.UserAggregate;
+
+public class User : RootAggregate
 {
     public string FirstName { get; } = null!;
     public string LastName { get; } = null!;
@@ -8,4 +10,24 @@ public class User
     public Guid? AdminId { get; private set; }
     public Guid? PlayerId { get; private set; }
     public Guid? InstructorId { get; private set; }
+    
+    private readonly string _passwordHash = null!;
+    
+    public User(string firstName,
+                string lastName,
+                string email,
+                string passwordHash,
+                Guid? adminId = null,
+                Guid? playerId = null,
+                Guid? instructorId = null,
+                Guid? id = null) : base(id ?? Guid.NewGuid())
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        AdminId = adminId;
+        PlayerId = playerId;
+        InstructorId = instructorId;
+        _passwordHash = passwordHash;
+    }
 }
