5974d6c7cabc6246d094acf92f6d8f6da2a26321
MartinBarrettNielsen11 <76744292+MartinBarrettNielsen11@users.noreply.github.com>
2026-06-07 16:07:31 +0200

    added background service for consuming integration events

---
+++ SessionBookingService/src/SessionBookingService.Infrastructure/Infrastructure/MessageBrokerSettings.cs
@@ -0,0 +1,13 @@
+namespace SessionBookingService.Infrastructure.Infrastructure;
+
+public class MessageBrokerSettings
+{
+    public const string Section = "MessageBroker";
+
+    public string HostName { get; init; } = null!;
+    public int Port { get; init; }
+    public string UserName { get; init; } = null!;
+    public string Password { get; init; } = null!;
+    public string QueueName { get; init; } = null!;
+    public string ExchangeName { get; init; } = null!;
+}
