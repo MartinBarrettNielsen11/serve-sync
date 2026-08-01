d86311ab4b71f871eb5544ccd7cd7eb538ea4049
MartinBarrettNielsen11 <76744292+MartinBarrettNielsen11@users.noreply.github.com>
2026-08-01 11:39:52 +0200

    init attempt of adding code-fix provider

--- Analyzers/SealedClassAnalyzer.cs
+++ Analyzers/SealedClassAnalyzer.cs
@@ -9,7 +9,7 @@ namespace Analyzers;
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
 public sealed class SealedClassAnalyzer : DiagnosticAnalyzer
 {
-    private const string RuleId = "RULE0001";
+    internal const string RuleId = "RULE0001";
 
     private static readonly DiagnosticDescriptor Rule = new(
         RuleId,
@@ -43,69 +43,5 @@ public sealed class SealedClassAnalyzer : DiagnosticAnalyzer
         context.ReportDiagnostic(diagnostic);
     }
 
-    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => [Rule];
+    internal override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => [Rule];
 }
-
-/*
-[DiagnosticAnalyzer(LanguageNames.CSharp)]
-public class SealedClassAnalyzer : DiagnosticAnalyzer
-{
-    internal const string RuleId = "RULE0001";
-
-    private static readonly DiagnosticDescriptor Rule = new(
-        RuleId,
-        title: "The class have to be sealed",
-        messageFormat: "Class can be sealed",
-        category: "Unknown",
-        DiagnosticSeverity.Warning,
-        isEnabledByDefault: true);
-
-    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
-        ImmutableArray.Create(_rule);
-
-    public override void Initialize(AnalysisContext context)
-    {
-        // You must call this method to avoid analyzing generated code.
-        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
-
-        // You must call this method to enable the Concurrent Execution.
-        context.EnableConcurrentExecution();
-
-        context.RegisterSyntaxNodeAction(AnalyzeSymbol, SyntaxKind.IdentifierName);
-    }
-
-    private static readonly string[] _forbiddenPropertyNames = new[]
-    {
-        nameof(DateTime.Now), nameof(DateTime.Today), nameof(DateTime.UtcNow)
-    };
-
-    private static void AnalyzeSymbol(SyntaxNodeAnalysisContext context)
-    {
-        SyntaxToken token = context.Node.GetFirstToken();
-
-        if (!token.ToString().Equals(nameof(DateTime), StringComparison.Ordinal))
-        {
-            return;
-        }
-
-        SyntaxToken nextToken = token.GetNextToken();
-
-        if (!nextToken.IsKind(SyntaxKind.DotToken))
-        {
-            return;
-        }
-
-        SyntaxToken dateTimeProperty = nextToken.GetNextToken();
-        foreach (var forbiddenPropertyName in _forbiddenPropertyNames)
-        {
-            if (!dateTimeProperty.ToString().Equals(forbiddenPropertyName, StringComparison.Ordinal))
-            {
-                continue;
-            }
-
-            context.ReportDiagnostic(Diagnostic.Create(_rule, context.Node.Parent!.GetLocation(), forbiddenPropertyName));
-            break;
-        }
-    }
-}
-*/
